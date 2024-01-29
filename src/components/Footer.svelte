cbc00271284ccc1debc695634cf2075184998259
JP5252 <justin.pearson52@hotmail.com>
2024-01-28 21:10:34 -0700

    add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,48 @@
+<footer
+  class="py-20 sm:py-32bg-black border-t border-solid border-green-950
+flex flex-col gap-4 sm:-gap8 justify-center items-center"
+>
+  <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    Connect with me &darr;
+  </p>
+  <div class="flex flex-col gap-4 items-center justify-center">
+    <p>
+      <i class="fa-regular fa-envelope"></i>
+      <b class="pr-2">Email</b>justin.pearson52@hotmail.com
+    </p>
+    <p>
+      <i class="fa-brands fa-github"></i>
+      <b class="pr-2">GitHub</b>
+      <a
+        class="text-green-400"
+        target="_blank"
+        href="https://github.com/JP5252"
+      >
+        JP5252<sup
+          ><span class="text-xs scale-75 pl-0.5">
+            <i
+              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+            />
+          </span></sup
+        >
+      </a>
+    </p>
+    <p>
+      <i class="fa-brands fa-linkedin"></i>
+      <b class="pr-2">Linkedin</b>
+      <a
+        class="text-green-400"
+        target="_blank"
+        href="www.linkedin.com/in/justin-pearson5252"
+      >
+        Justin Pearson<sup
+          ><span class="text-xs scale-75 pl-0.5">
+            <i
+              class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+            />
+          </span></sup
+        >
+      </a>
+    </p>
+  </div>
+</footer>
